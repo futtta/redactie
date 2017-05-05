0c9ef329613b695a9096fe4617dc57a598da0f2d
frank goossens <futtta@gmail.com>
2017-05-05 09:32:06 +0200

    Surely you don't need that SSH-tunnel

--- index.php
+++ index.php
@@ -388,8 +388,10 @@ function fetchUrl($url) {
             curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727)");
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+            /* you don't need to tunnel out, do you?
             curl_setopt($curl, CURLOPT_PROXY, "localhost:6789");
             curl_setopt($curl, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS4);
+            */
             $str = curl_exec($curl);
             $err = curl_error($curl);
             curl_close($curl);
